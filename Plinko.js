5b2e6de1b2085b5a5813f73e43445e6e354267e5
vaishviluhar <78733046+vaishviluhar@users.noreply.github.com>
2021-05-17 12:01:42 +0530

    Add files via upload

--- Plinko.js
+++ Plinko.js
@@ -1,18 +1,18 @@
 class Plinko{
     constructor(x, y, radius){
-        var optons = {
+        var options = {
             isStatic : true
         }
 
-        this.body = ellipse(x, y, radius);
+        this.body = Bodies.circle(x,y,radius,options);
         this.radius = radius;
         World.add(world, this.body);
     }
 
     display(){
         var pos =this.body.position;
-        ellipseMode(CENTER);
+        circleMode(CENTER);
         fill("brown");
-        ellipse(pos.x, pos.y, this.radius, this.radius);
+        circle(pos.x, pos.y, this.radius, this.radius);
       }
 }
