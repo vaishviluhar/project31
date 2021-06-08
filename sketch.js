9f1c9b9f97b82b698e9ec24ad9f2b91579885b10
vaishviluhar <78733046+vaishviluhar@users.noreply.github.com>
2021-06-08 12:06:21 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -20,37 +20,47 @@ function setup() {
   plinko = new Plinko(240, 400, 10);
 
   divisions = new Division(1, 580, 10, 400);
+
+  for (var j = 75; j <= width; j=j + 50){
+    plinkos.push(new Plinko(j, 75));
+  }
+  for (var j = 50; j <= width-10; j=j + 50){
+    plinkos.push(new Plinko(j, 175));
+  }
+  
+  for  (var j = 75; j <= width; j= j +  50){
+    plinkos.push(new Plinko(j, 275));
+  }
+
+  for (var j = 50; j <= width-10; j=j+50){
+    plinkos.push(new Plinko(j, 375));
+  }
 }
 
 function draw() {
+  background("black"); 
+  textSize(20);
   Engine.update(engine);
-  background(255,255,255); 
   
   ground.display();
   plinko.display();
   divisions.display();
-
-  for (var j = 40; j <= width; j=j + 50){
-    plinkos.push(new Plinko(j, 75));
+  
+  for (var i = 0; i < plinkos.length; i++){
+    plinkos[i].display();
   }
-  for (var j = 15; j <= width-10; j=j + 50){
-    plinkos.push(new Plinko(j, 175));
-  }
-
+  
   if (frameCount%90 === 0){
     particles.push(new Particle(random(width/ 2-10, width/ 2+10, 10, 10)));
+    score++;
   }
-
+  
   for (var j =0; j < particle.length; j++){
     particles[j].display();
   }
-
+  
   for (var k =0; k < divisions.length; k++){
     divisions[k].display();
-  }
-  
-  for  (var k = 0; k <= width; k= k +  80){
-    divisions.push(new Divisions(k, height - divisionHeight/2, 10, divisionHeight));
   }
 
   drawSprites();
