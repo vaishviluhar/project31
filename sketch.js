9b20e13c05542634734d35a89c373423c4868b4f
vaishviluhar <78733046+vaishviluhar@users.noreply.github.com>
2021-06-10 12:01:27 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-const Engine = Matter.Engine,
+var Engine = Matter.Engine,
   World = Matter.World,
   Events = Matter.Events,
   Bodies = Matter.Bodies;
@@ -8,12 +8,10 @@ var plinkos = [];
 var divisions =[];
 var divisionHeight=300;
 var score =0;
-
 function setup() {
   createCanvas(800, 800);
   engine = Engine.create();
   world = engine.world;
-
   ground = new Ground(width/2,height,width,20);
 
 
@@ -45,4 +43,37 @@ function setup() {
     
        plinkos.push(new Plinko(j,375));
     }
-  }
+
+    
+
+    
+}
+ 
+
+
+function draw() {
+  background("black");
+  textSize(20)
+ //text("Score : "+score,20,30);
+  Engine.update(engine);
+  ground.display();
+  
+   for (var i = 0; i < plinkos.length; i++) {
+     
+     plinkos[i].display();
+     
+   }
+   if(frameCount%60===0){
+     particles.push(new Particle(random(width/2-30, width/2+30), 10,10));
+     score++;
+   }
+ 
+  for (var j = 0; j < particles.length; j++) {
+   
+     particles[j].display();
+   }
+   for (var k = 0; k < divisions.length; k++) {
+     
+     divisions[k].display();
+   }
+}
